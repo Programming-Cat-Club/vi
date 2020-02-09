9a11276638878f062d5e62af8e87c6035227f817
冷鱼闲风 <39406598+2991883280@users.noreply.github.com>
2020-02-09 22:35:04 +0800

    的
    c'x出现

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+if(version_compare(PHP_VERSION,'5.4.0','<')){
+	header("Content-Type: text/html; charset=UTF-8");
+	die('HYPHP2.0 不支持 5.4以下的PHP版本.  当前你的PHP版本：' . PHP_VERSION);
+}
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+define('HYBBS_V'			,'2.3.2');
+define('INDEX_PATH' 		, str_replace('\\', '/', dirname(__FILE__)).'/');
+define('DEBUG'      ,(is_file(INDEX_PATH . 'DEBUG'))?false:true);
+define('PLUGIN_ON'  ,true);
+define('PLUGIN_ON_FILE',true);
+define('PLUGIN_MORE_LANG_ON',true);
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+require  'HY/HYPHP.php';
+/*
+
+archiver
+
+重新部署 互联登陆系统 统一页面 (例如 QQ 微信 等第三方登陆 统一页面)
+重新部署 充值系统 统一页面
+
+优化部分细节
+未完待续
+
+ */
